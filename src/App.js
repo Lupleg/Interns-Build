534ea4882cc5e5e2a91c64e5f2e70e9a278bb693
Nitin <nit8948@gmail.com>
2024-02-08 22:56:51 +0530

    Feature setion done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Button from "./components/Button";
+import Feature from "./components/Feature";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='border-2 py-10 font-bold  flex flex-col justify-center items-center text-center'>
+
+          <p className="md:text-4xl text-3xl ">A lot goes into keeping customers happy.</p>
+          <p className="md:text-4xl text-3xl">Keep it all in Front.</p>
+      
+          <Feature/>
+
+          <Button/>
+          
+      </div>
     </div>
   );
 }
