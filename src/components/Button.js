ba366c4aa2d74ea49a7d2cab535b1cc86b7e90cd
Nitin <nit8948@gmail.com>
2024-02-09 15:34:42 +0530

    NAvbar Structure done

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = () => {
   return (
     <div>
-        <button className='w-32  p-3 font-normal my-10 rounded-full bg-pink-500 text-white shadow-lg hover:shadow-pink-600 '>
+        <button className='w-32  p-3 font-normal my-4 rounded-full bg-pink-500 text-white shadow-lg hover:shadow-pink-600 '>
             Get started
         </button>
     </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useState } from 'react';
+import { Twirl as Hamburger } from 'hamburger-react'
+import Button from './Button';
+
+const Navbar = () => {
+    const [isOpen, setOpen] = useState(false);
+  return (
+    <div className='flex justify-between  w-4/5 md:w-11/12 lg:w-4/5 md:mx-auto mx-auto'>
+        <div className='flex gap-x-2 justify-center items-center '>
+            <img src='https://seeklogo.com/images/F/front-logo-5E94F18365-seeklogo.com.png' className='w-4 md:w-6 lg:w-8'></img>
+            <p className='text-xl md:text-2xl lg:text-4xl font-bold'>Front</p>
+        </div>
+        
+
+        <div className='lg:flex lg:justify-center lg:text-md lg:items-center lg:gap-x-6 lg:font-medium hidden lg:block'>
+            <a>Product</a>
+            <a>Solutions</a>
+            <a>Pricing</a>
+            <a>Resources</a>
+            <a>Contact Us</a>
+        </div>
+
+        <div className='lg:hidden'>
+            <Hamburger toggled={isOpen} toggle={setOpen} size={30}/>
+        </div>
+
+        <div className='hidden lg:block lg:flex lg:gap-x-8 lg:justify-center'>
+            <a className='text-md my-auto font-medium'>Sign In</a>
+            <Button/>
+            
+        </div>
+
+        
+
+    </div>
+  )
+}
+
+export default Navbar;
