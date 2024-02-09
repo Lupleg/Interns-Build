1314aeceaa927a76b30eef95c587f165dacf76cd
antonyosebe <osebe.anto@gmail.com>
2024-02-09 16:39:19 +0300

    Add grid resource component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-
+import './Resources';
+import Resources from './Resources';
 function App() {
   return (
     <div className="App">
       <section className='text-red-400'>
         <p> Welcome to intern build, let us get started, Innit</p>
+        <Resources/>
       </section>
     </div>
   );
