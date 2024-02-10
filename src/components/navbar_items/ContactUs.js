c817c5c0b0e436a84f19130a22fb6aa0c1753914
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2024-02-10 09:45:10 +0300

    Refactor `ContactUs.js`

--- src/components/navbar_items/ContactUs.js
+++ src/components/navbar_items/ContactUs.js
@@ -2,41 +2,38 @@ import React, { useState } from "react";
 
 const ContactUs = () => {
   const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <li className="relative">
-      <button
-        onMouseOver={() => setIsOpen(true)}
-        onMouseOut={() => setIsOpen(false)}
-        className="font-semibold py-2 px-4 rounded"
-      >
-        Contact Us
-      </button>
-      {isOpen && (
-        <div className="absolute top-10 left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-          <div
-            className="py-1"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="options-menu"
+  const Popup = () => {
+    return (
+      <div className="absolute top-10 left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+        <div
+          className="py-1"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          <button
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            role="menuitem"
           >
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              role="menuitem"
-            >
-              Talk to Sales
-            </a>
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              role="menuitem"
-            >
-              Email us for support
-            </a>
-          </div>
+            Talk to Sales
+          </button>
+          <button
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            role="menuitem"
+          >
+            Email us for support
+          </button>
         </div>
-      )}
+      </div>
+    );
+  };
+  return (
+    <li onMouseOver={() => setIsOpen(true)} onMouseOut={() => setIsOpen(false)}>
+      <li className="relative">
+        <button className="font-semibold py-2 px-4 rounded">
+          Contact Us {isOpen && Popup()}
+        </button>
+      </li>
     </li>
   );
 };
