b2ce7d3f8202e648ded6b83c2ca7a1af21a9f4d5
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2024-02-12 17:31:52 +0300

    Structure and populate popup menu

--- src/components/nav/Contact.jsx
+++ src/components/nav/Contact.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const Contact = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const Popup = () => {
+  const Menu = () => {
     return (
       <div className="absolute top-10 left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
         <div
@@ -11,29 +11,57 @@ const Contact = () => {
           aria-orientation="vertical"
           aria-labelledby="options-menu"
         >
-          <button
-            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            role="menuitem"
-          >
-            Talk to Sales
-          </button>
-          <button
-            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            role="menuitem"
-          >
-            Email us for support
-          </button>
+          <p>Get in touch</p>
+          <ul rows="2">
+            <li>
+              <button
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                role="menuitem"
+              >
+                <div>
+                  <p>
+                    <span>Talk to Sales</span>
+                    <div>
+                      Got a specific question for our Sales team? Get an answer here
+                    </div>
+                  </p>
+                </div>
+              </button>
+            </li>
+            <li>
+              <button
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                role="menuitem"
+              >
+                <div>
+                  <p>
+                    <span>Email us for support</span>
+                    <div>
+                      Need help using Front? Our team of product experts is here to troubleshoot
+                    </div>
+                  </p>
+                </div>
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
     );
   };
   return (
-    <li onMouseOver={() => setIsOpen(true)} onMouseOut={() => setIsOpen(false)}>
-      <li className="relative">
-        <button className="font-semibold py-2 px-4 rounded">
-          Contact Us {isOpen && Popup()}
-        </button>
-      </li>
+    <li >
+      <button className="relative" onMouseOver={() => setIsOpen(true)} onMouseOut={() => setIsOpen(false)}>
+        <span className="font-semibold py-2 px-4 rounded">
+          Contact Us
+        </span>
+        {/* <div> */}
+        {/*   <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="24"> */}
+        {/*     <path d="M18.872 15.136L12 9l-6.872 6.136" stroke="#001B38" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> */}
+        {/*     </path> */}
+        {/*   </svg> */}
+        {/* </div> */}
+        {isOpen && Menu()}
+      </button>
     </li>
   );
 };
