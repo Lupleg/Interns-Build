be2f2f8c81dea27c9661ddd28182d181b635fbb5
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-02-13 14:02:59 +0300

    Added the the new App.jsx from Nitin's

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Feature from "./components/features/Feature";
+import Header from "./components/features/Header";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Feature />
+    </div>
+  );
+}
+
+export default App;
