69b917f228ae8ced7bef606520d55c4885740ad6
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-02-14 12:36:35 +0300

    Copied Nitin's Navbarr.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,20 @@ import { useState } from "react";
 import { Twirl as Hamburger } from "hamburger-react";
 import Button from "./Button";
 import Contact from "./nav/Contact";
-import Solutions from "./nav/Solutions";
-
+import { RiArrowDropDownLine, RiArrowDropUpLine } from "react-icons/ri";
 
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
+  const toggleMenu = () => {
+    setOpen(!isOpen);
+  };
+
+  const [dropOpen, setDropOpen]= useState(false);
+  const toggleDrop = () => {
+    setDropOpen(!dropOpen);
+  };
   return (
+    <div>
     <div className="flex justify-between  w-4/5 md:w-11/12 lg:w-4/5 md:mx-auto mx-auto">
       <div className="flex gap-x-2 justify-center items-center ">
         <img
@@ -20,19 +28,42 @@ const Navbar = () => {
 
       <ul className="lg:flex lg:justify-center lg:text-md lg:items-center lg:gap-x-6 lg:font-medium hidden lg:block">
         <li>Product</li>
-        <Solutions/>
+        <li>Solutions</li>
         <li>Pricing</li>
         <li>Resources</li>
         <Contact />
       </ul>
+      
       <div className="lg:hidden">
         <Hamburger toggled={isOpen} toggle={setOpen} size={30} />
       </div>
 
+      
+
       <div className="hidden lg:block lg:flex lg:gap-x-8 lg:justify-center">
         <a className="text-md my-auto font-medium">Sign In</a>
-        <Button />
+        <Button/>
       </div>
+    </div>
+
+    <div className="flex lg:hidden block w-4/5 mx-auto mt-4">
+
+      {isOpen &&(
+        <div className="flex flex-col gap-y-8 w-full font-bold align-center justify-center">
+
+          <button className="flex  justify-between border-b-2 pb-3" onClick={toggleDrop}>Products {!dropOpen && <RiArrowDropDownLine size={30}/>} {dropOpen && <RiArrowDropUpLine size={30}/> } </button>
+          <button className="flex justify-between border-b-2 pb-3" onClick={toggleDrop}>Solutions{!dropOpen && <RiArrowDropDownLine size={30}/>} {dropOpen && <RiArrowDropUpLine size={30}/> }</button>
+          <button className="flex justify-between border-b-2 pb-3" onClick={toggleDrop}>Resources{!dropOpen && <RiArrowDropDownLine size={30}/>} {dropOpen && <RiArrowDropUpLine size={30}/> }</button>
+          <button className="flex justify-between border-b-2 pb-3">Pricing</button>
+          <button className="flex justify-between border-b-2 pb-3" onClick={toggleDrop}>Contacts{!dropOpen && <RiArrowDropDownLine size={30}/>} {dropOpen && <RiArrowDropUpLine size={30}/> }</button>
+          <button className="flex justify-between border-b-2 pb-3">Sign In</button>
+
+          <div className="mx-auto">  <Button/></div>
+          
+        </div>
+      )}
+      
+    </div>
     </div>
   );
 };
