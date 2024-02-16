67add32e320df7863e2f36e743fd381a71318a82
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-02-16 07:27:19 +0300

    Made changes on the cursor and font weight

--- src/components/nav/Solutions.jsx
+++ src/components/nav/Solutions.jsx
@@ -15,22 +15,22 @@ const Solutions = () => {
     <div
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className=''
+      className='cursor-pointer'
     >
       Solutions
       <div
         className={`pt-2
-        absolute md:top-16 px-0 py-2 left-1/2 transform -translate-x-1/2 ${isHovered ? "block" : "hidden"} w-3/4 bg-white shadow-lg rounded-md`}
+        absolute md:top-14 px-0 py-2 left-1/2 transform -translate-x-1/2 ${isHovered ? "block" : "hidden"} w-3/4 bg-white shadow-lg rounded-md`}
       >
         <div className='pt-5 flex flex-col lg:flex-row'>
           <div className='w-full lg:w-1/2 border-r border-gray-300'>
-            <h2 className='pl-5 text-lg text-red-600 font-semibold mb-2'>
+            <h2 className='pl-5 text-lg text-pink-600 font-semibold mb-2'>
               Industries
             </h2>
             <div className='flex flex-wrap'>
               <div className='p-2 md:w-1/2'>
                 <div className='hover:bg-gray-100 p-4 pb-3 bg-white rounded-md'>
-                  <h2 className='text-base font-semibold'>
+                  <h2 className='text-base font-bold'>
                     <a href='#'>Technology</a>
                   </h2>
                   <p className='text-gray-600 text-sm'>
@@ -41,7 +41,7 @@ const Solutions = () => {
 
               <div className='md:w-1/2 p-2'>
                 <div className='hover:bg-gray-100 p-4 pb-3 bg-white rounded-md'>
-                  <h2 className='text-base font-semibold'>
+                  <h2 className='text-base font-bold'>
                     <a href='#'>Financial Services</a>
                   </h2>
                   <p className='text-gray-600 text-sm'>
@@ -53,7 +53,7 @@ const Solutions = () => {
 
               <div className='md:w-1/2 p-2'>
                 <div className='hover:bg-gray-100 p-4 pb-3 bg-white rounded-md'>
-                  <h2 className='text-base font-semibold'>
+                  <h2 className='text-base font-bold'>
                     <a href='#'>Logistics</a>
                   </h2>
                   <p className='text-gray-600 text-sm'>
@@ -64,7 +64,7 @@ const Solutions = () => {
               </div>
               <div className='md:w-1/2 p-2'>
                 <div className='hover:bg-gray-100 p-4 pb-3 bg-white rounded-md'>
-                  <h2 className='text-base font-semibold'>
+                  <h2 className='text-base font-bold'>
                     <a href='#'>Manufacturing</a>
                   </h2>
                   <p className='text-gray-600 text-sm'>
@@ -85,7 +85,7 @@ const Solutions = () => {
               </div>
               <div className='md:w-1/2 p-2'>
                 <div className='hover:bg-gray-100 p-4 pb-3 bg-white rounded-md'>
-                  <h2 className='text-base font-semibold'>
+                  <h2 className='text-base font-bold'>
                     <a href='#'>Travel</a>
                   </h2>
                   <p className='text-gray-600 text-sm'>
@@ -98,13 +98,13 @@ const Solutions = () => {
           </div>
 
           <div className="w-full lg:w-1/2">
-            <h2 className='pl-5 text-lg text-red-600 font-semibold mb-2'>
+            <h2 className='pl-5 text-lg text-pink-600 font-semibold mb-2'>
               Teams
             </h2>
             <div className='flex flex-wrap'>
               <div className='w-full sm:w-1/2 md:w-1/2 p-2'>
                 <div className='hover:bg-gray-100 p-3 bg-white rounded-md'>
-                  <h2 className='text-base font-semibold'>
+                  <h2 className='text-base font-bold'>
                     <a href='#'>Customer Services and Support</a>
                   </h2>
                   <p className='text-gray-600 text-sm'>
@@ -115,7 +115,7 @@ const Solutions = () => {
 
               <div className='w-full sm:w-1/2 md:w-1/2 p-2'>
                 <div className='hover:bg-gray-100 p-3 bg-white rounded-md'>
-                  <h2 className='text-base font-semibold'>
+                  <h2 className='text-base font-bold'>
                     <a href='#'>Operations</a>
                   </h2>
                   <p className='text-gray-600 text-sm'>
@@ -125,7 +125,7 @@ const Solutions = () => {
               </div>
               <div className='w-full sm:w-1/2 md:w-1/2 p-2'>
                 <div className='hover:bg-gray-100 p-3 bg-white rounded-md'>
-                  <h2 className='text-base font-semibold'>
+                  <h2 className='text-base font-bold'>
                     <a href='#'>Inbound Sales</a>
                   </h2>
                   <p className='text-gray-600 text-sm'>
@@ -135,7 +135,7 @@ const Solutions = () => {
               </div>
               <div className='w-full sm:w-1/2 md:w-1/2 p-2'>
                 <div className='hover:bg-gray-100 p-3 bg-white rounded-md'>
-                  <h2 className='text-base font-semibold'>
+                  <h2 className='text-base font-bold'>
                     <a href='#'>Account Management</a>
                   </h2>
                   <p className='text-gray-600 text-sm'>
@@ -145,7 +145,7 @@ const Solutions = () => {
               </div>
               <div className='w-full sm:w-1/2 md:w-1/2 p-2'>
                 <div className='hover:bg-gray-100 p-3 bg-white rounded-md'>
-                  <h2 className='text-base font-semibold'>
+                  <h2 className='text-base font-bold'>
                     <a href='#'>Success</a>
                   </h2>
                   <p className='text-gray-600 text-sm'>
