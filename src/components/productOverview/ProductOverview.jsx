7eec9a017b328cc063789e35dd99e476b9ef82c7
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-02-17 09:21:48 +0300

    made changes on the product overview

--- src/components/productOverview/ProductOverview.jsx
+++ src/components/productOverview/ProductOverview.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ExploreCapabilities from './ExploreCapabilities'
+import IntegrateApps from './IntegrateApps'
 
 const ProductOverview = () => {
   return (
     <div>
       <ExploreCapabilities/>
+      <IntegrateApps/>
     </div>
   )
 }
