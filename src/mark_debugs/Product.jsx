bfc12a289e609c79c474149a80982c6c46b3c1d7
marksikaundi <emmanuelmark117@gmail.com>
2024-02-17 07:59:52 +0200

    hillary component layout

---
+++ src/mark_debugs/Product.jsx
@@ -0,0 +1,70 @@
+
+// check here @HillaryNyakundi
+export default function Component() {
+    return (
+      <div className="bg-[#F9FAFB] py-16 bg-[#F9FAFB]">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              Explore all of Front's capabilities
+            </h2>
+            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Learn more about our product →</p>
+          </div>
+          <div className="mt-10">
+            <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+              <div className="relative">
+                <dt>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">Collaboration →</p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Work more efficiently with next-level team coordination
+                </dd>
+              </div>
+              <div className="relative">
+                <dt>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">Workflow automation →</p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Organize, route, and resolve conversations at scale
+                </dd>
+              </div>
+              <div className="relative">
+                <dt>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">Omnichannel inbox →</p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Manage shared inboxes for all channels in one place
+                </dd>
+              </div>
+              <div className="relative">
+                <dt>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">Analytics →</p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Inform decisions with team workflows, insights, and team performance
+                </dd>
+              </div>
+              <div className="relative">
+                <dt>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">Knowledge Base →</p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Resolve customer inquiries faster with smart deflection
+                </dd>
+              </div>
+              <div className="relative">
+                <dt>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">Live chat →</p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Support customers on your website or app in real time
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  
