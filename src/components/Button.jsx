c683d1a9dd32d6b668edbb62d91178fa1096dc10
Nitin <nit8948@gmail.com>
2024-02-19 22:36:34 +0530

    information section added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({text,width,height}) => {
   return (
     <div>
-      <button className={`w-${width} p-3 h-${height} font-normal my-4 rounded-full text-white shadow-lg bg-pink-500  hover:shadow-pink-500 `} >
+      <button className={`w-${width} p-3 px-6 h-${height} font-normal my-4 rounded-full text-white shadow-lg bg-pink-500  hover:shadow-pink-500 `} >
         {text}
       </button>
     </div>
--- src/components/productOverview/ProductOverview.jsx
+++ src/components/productOverview/ProductOverview.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import IntegrateApps from './IntegrateApps'
 import Footer from '../Footer'
+import Informative from './Informative'
 
 const productOverview = () => {
   return (
-    <div>
+    <div className=''>
         <IntegrateApps/>
+        <Informative text="In comes Front. Wow every customer with fast, accurate, and human responses."/>
+        <Informative text="Streamline your customer communication with Front"/>
         <Footer/>
     </div>
   )
