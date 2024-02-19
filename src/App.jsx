ebfeb299de2615de13b51f533c81eefbb36a541c
Vikram <vikram.gupta.2513@gmail.com>
2024-02-20 03:09:21 +0530

    Slider added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/features/Header";
 import Navbar from "./components/Navbar";
 import NotAnd from "./components/features/NotAnd";
 import Helpdesk from "./components/features/Helpdesk";
-
+import Slider from "./components/features/Slider";
 import Footer from "./components/features/Footer";
 
 function App() {
@@ -12,15 +12,13 @@ function App() {
       <Navbar />
       <Header />
       <Feature />
+      <Slider />
       <NotAnd />
       <Helpdesk />
-    
-      <Footer/>
-      
-     
-          {/* <Route path="product" element={<ProductOverview/>}></Route> */}
-        
-      
+
+      <Footer />
+
+      {/* <Route path="product" element={<ProductOverview/>}></Route> */}
     </div>
   );
 }
