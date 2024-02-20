e28ef6884ea11692b9c14c91b23550be5d5807d7
Nitin <nit8948@gmail.com>
2024-02-20 13:54:24 +0530

    Vikram's slider added

--- src/components/productOverview/ProductOverview.jsx
+++ src/components/productOverview/ProductOverview.jsx
@@ -3,12 +3,14 @@ import IntegrateApps from './IntegrateApps'
 import Footer from '../Footer'
 import Informative from './Informative'
 import CardCrousel from './CardCrousel'
+import Slider from './Slider'
 
 const productOverview = () => {
   return (
     <div className=''>
         <IntegrateApps/>
         <Informative text="In comes Front. Wow every customer with fast, accurate, and human responses."/>
+        <Slider/>
         <CardCrousel/>
         <Informative text="Streamline your customer communication with Front"/>
         
