1fc387f25142fd9506a9384791bbea8a84d417a4
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2024-02-20 21:50:23 +0300

    Update src/components/productOverview/ProductOverview.jsx

---
+++ src/components/productOverview/ProductOverview.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ExploreCapabilities from './ExploreCapabilities'
+import IntegrateApps from './IntegrateApps'
+
+const ProductOverview = () => {
+  return (
+    <div>
+      <ExploreCapabilities/>
+      <IntegrateApps/>
+    </div>
+  )
+}
+
+export default ProductOverview
