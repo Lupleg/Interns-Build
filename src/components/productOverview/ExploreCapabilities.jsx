9e2b1b6cbe87b393aff1e34f3d754ca1c5379b90
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-02-20 13:02:06 +0300

    Added the integrations and Api section

--- src/components/productOverview/ExploreCapabilities.jsx
+++ src/components/productOverview/ExploreCapabilities.jsx
@@ -9,17 +9,15 @@ const ExploreCapabilities = () => {
           <h2 className='text-3xl font-extrabold tracking-tight text-white sm:text-4xl'>
             Explore all of Front's capabilities
           </h2>
-          
+
           <p className='mt-4 max-w-2xl lg:text-xl sm:text-base text-white'>
             Learn more about our product
             <span className='inline-block px-2 align-middle'>
               <a href=''>
-                <HiArrowSmRight size={25}/>
+                <HiArrowSmRight size={25} />
               </a>
             </span>
           </p>
-      
-          
         </div>
         <div className='mt-16 pb-8 sm:ml-4'>
           <dl className='space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10'>
@@ -59,7 +57,7 @@ const ExploreCapabilities = () => {
                   Omnichannel inbox
                   <span className='inline-block pl-2 px-2 align-middle'>
                     <a href=''>
-                      <HiArrowSmRight/>
+                      <HiArrowSmRight />
                     </a>
                   </span>
                 </p>
@@ -74,7 +72,7 @@ const ExploreCapabilities = () => {
                   Analytics
                   <span className='inline-block pl-2 px-2 align-middle'>
                     <a href=''>
-                      <HiArrowSmRight/>
+                      <HiArrowSmRight />
                     </a>
                   </span>
                 </p>
@@ -84,13 +82,13 @@ const ExploreCapabilities = () => {
                 performance
               </dd>
             </div>
-            <div className='relative lg:ml-14'>
+            <div className='relative lg:ml-14 pb-8'>
               <dt>
                 <p className='text-xl font-medium leading-6 text-white'>
                   Knowledge Base
                   <span className='inline-block pl-2 px-2 align-middle'>
                     <a href=''>
-                      <HiArrowSmRight/>
+                      <HiArrowSmRight />
                     </a>
                   </span>
                 </p>
@@ -99,7 +97,8 @@ const ExploreCapabilities = () => {
                 Resolve customer inquiries faster with smart deflection
               </dd>
             </div>
-            <div className='relative lg:ml-14'>
+
+            <div className='relative lg:ml-14 pb-8'>
               <dt>
                 <p className='text-xl font-medium leading-6 text-white'>
                   Live chat
@@ -112,6 +111,21 @@ const ExploreCapabilities = () => {
               </dt>
               <dd className='mt-2 text-base text-white'>
                 Support customers on your website or app in real time
+              </dd>
+            </div>
+            <div className='relative lg:ml-14'>
+              <dt>
+                <p className='text-xl font-medium leading-6 text-white'>
+                  Integrations & API
+                  <span className='inline-block pl-2 px-2 align-middle'>
+                    <a href=''>
+                      <HiArrowSmRight />
+                    </a>
+                  </span>
+                </p>
+              </dt>
+              <dd className='mt-2 text-base text-white'>
+                Seemlessly connect other systems to sync data, context and channels
               </dd>
             </div>
           </dl>
