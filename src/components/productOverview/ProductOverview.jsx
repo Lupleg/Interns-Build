7b5069eb5b02064cd770f3005b4a3fafe08d7391
Nitin <nit8948@gmail.com>
2024-02-20 13:21:51 +0530

    Crousel added successfully in ProductOverview Page

--- src/components/productOverview/ProductOverview.jsx
+++ src/components/productOverview/ProductOverview.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import IntegrateApps from './IntegrateApps'
 import Footer from '../Footer'
 import Informative from './Informative'
+import CardCrousel from './CardCrousel'
 
 const productOverview = () => {
   return (
     <div className=''>
         <IntegrateApps/>
         <Informative text="In comes Front. Wow every customer with fast, accurate, and human responses."/>
+        <CardCrousel/>
         <Informative text="Streamline your customer communication with Front"/>
+        
         <Footer/>
     </div>
   )
