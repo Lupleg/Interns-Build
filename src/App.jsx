0424d4f1de433996966c5222dca0f7314d9dbec0
Vikram <vikram.gupta.2513@gmail.com>
2024-02-21 17:12:06 +0530

    improve

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-/* eslint-disable no-unused-vars */
-import Feature from "./components/features/Feature";
-import Header from "./components/features/Header";
-import Navbar from "./components/Navbar";
-import NotAnd from "./components/features/NotAnd";
-import Helpdesk from "./components/features/Helpdesk";
-
 import ProductOverview from "./components/productOverview/ProductOverview";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
