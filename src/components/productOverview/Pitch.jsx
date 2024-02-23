09df7644bcfa1af6fc5b4cb407db5dcf42d29cb9
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2024-02-23 18:29:54 +0300

    Reduce margin and header font size for smaller screens

--- src/components/productOverview/Pitch.jsx
+++ src/components/productOverview/Pitch.jsx
@@ -12,27 +12,27 @@ const Pitch = ({ pitchItem }) => {
   };
 
   return (
-    <div className="m-20">
+    <div className="md:m-20 m-8">
       <div className="md:grid md:grid-flow-col md:grid-cols-12 md:gap-x-5 text-[rgb(0,27,56)]">
         <div className="md:flex md:flex-col mb-[30px] md:mb-[60px] md:ml-auto md:mr-auto md:col-start-3 md:col-end-[-3] md:row-start-1 md:row-end-auto md:gap-[1.875rem] text-center max-w-[58rem]">
-          <h2 className="text-[2rem]/[2.375rem] font-semibold tracking-[-0.01em] md:m-0 md:ms-0">
+          <h2 className="text-[1.5rem]/[1.875rem] md:text-[2rem]/[2.375rem] font-semibold tracking-[-0.01em] md:m-0 md:ms-0">
             {pitchItem.heading}
           </h2>
         </div>
-        <div className="mb-0 md:col-start-7 md:col-end-[-1] md:self-start md:overflow-hidden">
+        <div className="mb-0 md:col-start-7 md:col-end-[-1] md:self-start overflow-hidden">
           {vid && (
             <video
               autoPlay={true}
               muted={true}
               loop={true}
-              className="rounded-lg md:w-full md:max-w-full md:h-auto md:align-middle md:object-contain md:overflow-clip md:overflow-clip-margin"
+              className="rounded-lg md:w-full md:max-w-full md:h-auto align-middle md:object-contain md:overflow-clip md:overflow-clip-margin"
             >
               <source src={vid} />
             </video>
           )}
           {image && (
             <img
-              className="rounded-lg md:w-full md:max-w-full md:h-auto md:align-middle md:object-contain md:overflow-clip md:overflow-clip-margin"
+              className="rounded-lg md:w-full md:max-w-full md:h-auto align-middle md:object-contain md:overflow-clip md:overflow-clip-margin"
               src={image}
             />
           )}
