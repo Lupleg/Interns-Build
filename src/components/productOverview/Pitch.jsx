12b130a116d3619bd1eecf63a61a40495b84bf3e
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2024-02-23 18:45:44 +0300

    Sort tailwind classes

--- src/components/productOverview/Pitch.jsx
+++ src/components/productOverview/Pitch.jsx
@@ -12,45 +12,46 @@ const Pitch = ({ pitchItem }) => {
   };
 
   return (
-    <div className="md:m-20 m-8">
-      <div className="md:grid md:grid-flow-col md:grid-cols-12 md:gap-x-5 text-[rgb(0,27,56)]">
-        <div className="md:flex md:flex-col mb-[30px] md:mb-[60px] md:ml-auto md:mr-auto md:col-start-3 md:col-end-[-3] md:row-start-1 md:row-end-auto md:gap-[1.875rem] text-center max-w-[58rem]">
-          <h2 className="text-[1.5rem]/[1.875rem] md:text-[2rem]/[2.375rem] font-semibold tracking-[-0.01em] md:m-0 md:ms-0">
+    <div className="m-8 md:m-20">
+      <div className="text-[rgb(0,27,56)] md:grid md:grid-flow-col md:grid-cols-12 md:gap-x-5">
+        <div className="mb-[30px] max-w-[58rem] text-center md:col-start-3 md:col-end-[-3] md:row-start-1 md:row-end-auto md:mb-[60px] md:ml-auto md:mr-auto md:flex md:flex-col md:gap-[1.875rem]">
+          <h2 className="text-[1.5rem]/[1.875rem] font-semibold tracking-[-0.01em] md:m-0 md:ms-0 md:text-[2rem]/[2.375rem]">
             {pitchItem.heading}
           </h2>
         </div>
-        <div className="mb-0 md:col-start-7 md:col-end-[-1] md:self-start overflow-hidden">
+        <div className="mb-0 overflow-hidden md:col-start-7 md:col-end-[-1] md:self-start">
           {vid && (
             <video
               autoPlay={true}
               muted={true}
               loop={true}
-              className="rounded-lg md:w-full md:max-w-full md:h-auto align-middle md:object-contain md:overflow-clip md:overflow-clip-margin"
+              className="md:overflow-clip-margin rounded-lg align-middle md:h-auto md:w-full md:max-w-full md:overflow-clip md:object-contain"
             >
               <source src={vid} />
             </video>
           )}
           {image && (
             <img
-              className="rounded-lg md:w-full md:max-w-full md:h-auto align-middle md:object-contain md:overflow-clip md:overflow-clip-margin"
+              className="md:overflow-clip-margin rounded-lg align-middle md:h-auto md:w-full md:max-w-full md:overflow-clip md:object-contain"
               src={image}
+              alt=""
             />
           )}
         </div>
         <div className="md:col-span-6">
-          <ol className="md:flex md:flex-col md:gap-[30px] list-none md:pr-24 my-[30px] md:my-4 md:mx-0">
+          <ol className="my-[30px] list-none md:mx-0 md:my-4 md:flex md:flex-col md:gap-[30px] md:pr-24">
             {pitchItem.body.map((item, index) => (
               <li
-                className={`list-item list-none pl-3 border-l-4 border-solid ${index === expanded ? "border-[rgb(0,27,56)]" : "border-transparent"}`}
+                className={`list-item list-none border-l-4 border-solid pl-3 ${index === expanded ? "border-[rgb(0,27,56)]" : "border-transparent"}`}
               >
                 <p
-                  className="md:text-[1.5rem]/[2.125rem] font-semibold cursor-pointer my-4 md:mx-0"
+                  className="my-4 cursor-pointer font-semibold md:mx-0 md:text-[1.5rem]/[2.125rem]"
                   onClick={() => handleExpand(index, item.vid, item.image)}
                 >
                   {item.p}
                 </p>
                 {index === expanded && (
-                  <div className="text-[14px]/[1.25rem] md:text-[16px]/[1.5rem] mt-[20px]">
+                  <div className="mt-[20px] text-[14px]/[1.25rem] md:text-[16px]/[1.5rem]">
                     {item.div}
                   </div>
                 )}
