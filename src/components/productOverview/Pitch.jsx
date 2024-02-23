99185b965238927e7c045bba302f378c3d8c88cd
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2024-02-23 17:41:30 +0300

    Add responsiveness for screens less than md

--- src/components/productOverview/Pitch.jsx
+++ src/components/productOverview/Pitch.jsx
@@ -12,45 +12,45 @@ const Pitch = ({ pitchItem }) => {
   };
 
   return (
-    <div className="block m-20">
-      <div className="grid grid-flow-col grid-cols-12 gap-x-5 text-[rgb(0,27,56)]">
-        <div className="flex flex-col mb-[60px] ml-auto mr-auto col-start-3 col-end-[-3] row-start-1 row-end-auto gap-[1.875rem] text-center max-w-[58rem]">
-          <h2 className="text-[2rem]/[2.375rem] font-semibold tracking-[-0.01em] m-0 block ms-0">
+    <div className="m-20">
+      <div className="md:grid md:grid-flow-col md:grid-cols-12 md:gap-x-5 text-[rgb(0,27,56)]">
+        <div className="md:flex md:flex-col mb-[30px] md:mb-[60px] md:ml-auto md:mr-auto md:col-start-3 md:col-end-[-3] md:row-start-1 md:row-end-auto md:gap-[1.875rem] text-center max-w-[58rem]">
+          <h2 className="text-[2rem]/[2.375rem] font-semibold tracking-[-0.01em] md:m-0 md:ms-0">
             {pitchItem.heading}
           </h2>
         </div>
-        <div className="mb-0 block col-start-7 col-end-[-1] self-start rounded-[10px] overflow-hidden">
+        <div className="mb-0 md:col-start-7 md:col-end-[-1] md:self-start md:overflow-hidden">
           {vid && (
             <video
               autoPlay={true}
               muted={true}
               loop={true}
-              className="block w-full max-w-full h-auto align-middle object-contain overflow-clip overflow-clip-margin"
+              className="rounded-lg md:w-full md:max-w-full md:h-auto md:align-middle md:object-contain md:overflow-clip md:overflow-clip-margin"
             >
               <source src={vid} />
             </video>
           )}
           {image && (
             <img
-              className="block w-full max-w-full h-auto align-middle object-contain overflow-clip overflow-clip-margin"
+              className="rounded-lg md:w-full md:max-w-full md:h-auto md:align-middle md:object-contain md:overflow-clip md:overflow-clip-margin"
               src={image}
             />
           )}
         </div>
-        <div className="block col-span-6">
-          <ol className="flex flex-col gap-[30px] list-none pr-24 my-4 mx-0">
+        <div className="md:col-span-6">
+          <ol className="md:flex md:flex-col md:gap-[30px] list-none md:pr-24 my-[30px] md:my-4 md:mx-0">
             {pitchItem.body.map((item, index) => (
               <li
                 className={`list-item list-none pl-3 border-l-4 border-solid ${index === expanded ? "border-[rgb(0,27,56)]" : "border-transparent"}`}
               >
                 <p
-                  className="block text-[1.5rem]/[2.125rem] font-semibold cursor-pointer my-4 mx-0"
+                  className="md:text-[1.5rem]/[2.125rem] font-semibold cursor-pointer my-4 md:mx-0"
                   onClick={() => handleExpand(index, item.vid, item.image)}
                 >
                   {item.p}
                 </p>
                 {index === expanded && (
-                  <div className="block text-[16px]/[1.5rem] mt-[20px]">
+                  <div className="text-[14px]/[1.25rem] md:text-[16px]/[1.5rem] mt-[20px]">
                     {item.div}
                   </div>
                 )}
