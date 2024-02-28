fe5c412ca88150d35dea474e5d743eb6bf62cfe1
Aman-kumar21 <amansahil40@gmail.com>
2024-02-28 14:27:02 +0530

    Responsiveness for Product sub-menu

--- src/components/nav/Productprops.jsx
+++ src/components/nav/Productprops.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 const Productprops = (props) => {
   return (
     <div className='mt-4 mr-5'>
-        <p className='font-bold md:text-sm text-xs'>{props.heading}</p>
-        <p className='my-1 font: weight-80  md:text-sm text-[10px]'>{props.content}</p>
+        <p className='font-bold md:text-sm text-sm'>{props.heading}</p>
+        <p className='my-1    text-gray-600 text-sm'>{props.content}</p>
     </div>
   )
 }
